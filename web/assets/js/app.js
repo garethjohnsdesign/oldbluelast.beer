b10a9093fb2167ad8a06298fde98e62e800c1679
Gareth Johns <info@garethjohndesign.com>
2018-04-04 00:19:45 +0100

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -17,4 +17,56 @@ $("#gallery").lightGallery({
     selector: '.about__images',
     download: false
 });
-  
+
+$(function () {
+  count = 0;
+  wordsArray = ["Event", "Party", "Gig", "Karaoke", "Quiz", "Beer"];
+  setInterval(function () {
+    count++;
+    $("#photos-word").fadeOut(0, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(0);
+    });
+  }, 500);
+});
+
+$(function () {
+  count = 0;
+  eventsWordsArray = ["Events", "Parties", "Gigs", "Karaokes", "Quizes", "Festivals"];
+  setInterval(function () {
+    count++;
+    $("#events-word").fadeOut(0, function () {
+      $(this).text(eventsWordsArray[count % eventsWordsArray.length]).fadeIn(0);
+    });
+  }, 500);
+});
+
+
+var heroTimeline = anime.timeline({
+  loop: true,
+});
+
+heroTimeline
+  .add({
+    targets: '#heroTimeline .hero-one',
+    duration: 1,
+    opacity: 1,
+    delay: 500,
+  })
+  .add({
+    targets: '#heroTimeline .hero-two',
+    duration: 1,
+    opacity: 1,
+    delay: 500,
+  })
+  .add({
+    targets: '#heroTimeline .hero-three',
+    duration: 1,
+    opacity: 1,
+    delay: 500,
+  })
+    .add({
+    targets: '#heroTimeline .hero-four',
+    duration: 500,
+    opacity: 1,
+    delay: 500,
+  });
