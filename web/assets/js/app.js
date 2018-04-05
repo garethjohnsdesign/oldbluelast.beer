946ab91ac53e6f7557fd93e01f7698d8dc8ab890
Gareth Johns <info@garethjohndesign.com>
2018-04-05 01:32:45 +0100

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -70,3 +70,53 @@ heroTimeline
     opacity: 1,
     delay: 500,
   });
+
+
+
+$(".hamburger").click(function(){
+  $(this).toggleClass("is-active");
+});
+
+function checkIfVerified(){
+  if ($.cookie('age-gate') == 'of_age') {
+    // Hide overlay
+    $('.age-gate').hide();
+  } else {
+    // Show Age gate form
+    $('form[name=agegate]').show();
+  }
+}
+
+function zeroPadDate(number){
+  if (number < 10) { return '0'+number; }
+  return number;
+}
+
+checkIfVerified();
+
+$('form[name=agegate]').submit(function(ev){
+  ev.preventDefault();
+  var year = $('form[name=agegate] > select[name=year]').val();
+  var month = $('form[name=agegate] > select[name=month]').val();
+  var day = $('form[name=agegate] > select[name=day]').val();
+  
+  if (!(day && month && year)) {
+//     alert('Please enter your date of birth');
+    checkIfVerified();
+    return false;
+  }
+  
+  var date_string = year +  "-" + zeroPadDate(month) + "-" + zeroPadDate(day) + "T00:00:00";
+  var birthday = new Date(date_string);
+  var eighteenYearOld = new Date();
+  eighteenYearOld.setFullYear(eighteenYearOld.getFullYear() - 18);
+  
+  if ((eighteenYearOld - birthday) < 0) {
+    window.location.replace('https://www.drinkaware.co.uk/why-am-i-here');
+  } else {
+    $.cookie('age-gate', 'of_age');
+  }
+  
+  checkIfVerified();  
+  return false;
+});
