0c771371812222ba361618ae97b59eb8a0daabfd
Gareth Johns <garethjohns@Gareths-iMac.local>
2018-08-02 12:21:06 +0100

    Initial Commit

--- config/country-redirect.php
+++ config/country-redirect.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Country Redirect plugin for Craft CMS 3.x
+ *
+ * Easily redirect visitors to a locale based on their country of origin
+ *
+ * @link      https://superbig.co
+ * @copyright Copyright (c) 2017 Superbig
+ */
+/**
+ * Country Redirect config.php
+ *
+ * This file exists only as a template for the Country Redirect settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'country-redirect.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
 return [
     '*' => [
         /*
@@ -12,43 +33,33 @@ return [
             'loggedIn'  => true,
             'anonymous' => true,
         ],
-
-        /*
-         * Enable logging
-         */
-        'enableLogging'          => true,
-
         /*
          * Don't redirect bots/crawlers like GoogleBot, Bing etc.
          */
         'ignoreBots'          => true,
-
         /*
          * Add any special URL segments you want to ignore
          * Eg. 'ignoreSegments' => [
-         *     'this/page/only/exists/in/one/site',
+         *     'this/page/only/exists/in/one/locale',
          *     'global-page'
+         *     'fr/'
          * ],
          */
-        'ignoreSegments'      => [ ],
-
+        'ignoreSegments'      => [],
         /*
          * Cookie name
          */
         'cookieName'          => 'countryRedirect',
-
         /*
          * The URL parameter that let a user manually select which locale they want to see
          */
         'overrideLocaleParam' => 'selected-locale',
-
         /*
          * The URL parameter that indicates that a user was redirect
          */
-        'redirectedParam' => 'redirected',
-
+        'redirectedParam'     => 'redirected',
         /*
-         * Map a countrys two-letter ISO code to a Craft Site Handle, and/or define a catch-all with a * asterix
+         * Map a country's two-letter ISO code to a Craft Site Handle, and/or define a catch-all with a * asterix
          * Here is a list of ISO country codes: http://www.nationsonline.org/oneworld/country_code_list.htm
          * Example:
          * 'countryMap'       => [
@@ -59,7 +70,7 @@ return [
          *   '*' => 'default',
          * ]
          *
-         * If you within a country have different regional languages, you can map the different languages to sites by their Site Handle.
+         * If you within a country have different regional languages, you can map the different languages to locales.
          * Take Switzerland, with German, French, Italian and Romansh, as an example:
          *
          * 'countryMap'       => [
@@ -73,21 +84,23 @@ return [
          * This works by checking the Accept-Language header of the browser.
          *
          */
-        'countryMap'          => [ ],
-
+         'countryMap'       => [
+             'UK' => 'uk',
+             'US' => 'usa',
+             '*' => 'uk',
+         ],
         /*
-         * If you want to show a banner that prompts visitors to their matching site handle instead of redirecting them,
+         * If you want to show a banner that prompts visitors to their matching locale instead of redirecting them,
          * you can define these here.
          *
-         * The key here is the Craft Site Handle, not the country code. The variables {countryName} and {url} will
+         * The key here is the Craft locale id, not the country code. The variables {countryName} and {url} will
          * be replaced.
          *
          * 'banners' => [
-         *   'frenchSite' => 'It looks like your visiting from {countryName}. Do you <a href="{url}">want to visited the international site?</a>'
+         *   'en_us' => 'It looks like your visiting from {countryName}. Do you <a href="{url}">want to visited the international site?</a>'
          * ],
          */
-        'banners'             => [ ],
-
+        'banners'             => [],
         /*
          * Override the detected IP - useful for testing, or when no IP address can be detected
          */
