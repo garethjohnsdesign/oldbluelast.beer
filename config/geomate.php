de094f968d137402ca89dd99c20af2093fe986c1
Gareth Johns <info@garethjohnsdesign.com>
2019-01-18 23:46:40 +0000

    Geotargeting Amends

---
+++ config/geomate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ */
+
+return [
+  
+  // UK
+//   'forceIp' => '5.152.197.179',
+
+  // USA
+//   'forceIp' => '64.90.164.158',
+
+  'autoRedirectEnabled' => true,
+
+  'redirectMap' => [
+      'usa' => 'US',
+      'uk' => 'GB'
+  ],
+    
+];
+
+
+
