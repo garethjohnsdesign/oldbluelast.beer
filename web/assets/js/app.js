7dabbe781364711c54733e10453fc66b04bb124f
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-06-17 16:15:06 +0100

    Use Yarn Over Bower

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,3 +1,10 @@
+// @codekit-prepend "/../node_modules/jquery/dist/jquery.js";
+// @codekit-prepend "/../node_modules/foundation-sites/dist/js/foundation.js";
+// @codekit-prepend "/../node_modules/motion-ui/dist/motion-ui.js";
+// @codekit-prepend "/../node_modules/what-input/dist/what-input.js";
+// @codekit-prepend "/../node_modules/aos/dist/aos.js";
+// @codekit-prepend "/../node_modules/lightgallery/dist/js/lightgallery-all.js";
+
 Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
