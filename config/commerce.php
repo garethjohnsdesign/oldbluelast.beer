177736489bfe5987561ce755d351a728021e30a8
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-11-04 17:20:04 +0000

    Initial Commit

---
+++ config/commerce.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // ...
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        'purgeInactiveCartsDuration' => P1D,
+        // ...
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // ...
+    ],
+
+    // Production environment settings
+    'production' => [
+        // ...
+    ],
+];
+
+
+
