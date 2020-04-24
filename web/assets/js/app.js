c811c1cdc920203e6c5252a539e281ad85c781bd
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-04-24 17:23:06 +0100

    Code tidy

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,10 +3,21 @@
 
 import './jqueryload';
 import 'what-input';
+import mapboxgl from 'mapbox-gl';
+import AOS from 'aos';
 import Swiper from 'swiper';
 import Cookies from 'js-cookie';
 import './lib/foundation-explicit-pieces';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min';
+import Swup from 'swup';
+import SwupBodyClassPlugin from "@swup/body-class-plugin";
+import SwupScrollPlugin from '@swup/scroll-plugin';
+import SwupGaPlugin from '@swup/ga-plugin';
+import SwupPreloadPlugin from '@swup/preload-plugin';
+
+// Start Loop
+$( document ).ready( function() {
+  function init() {
 
 // 2. Foundation Special Queries
 // ------------------
@@ -21,41 +32,47 @@ Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min
 
 $(document).foundation();
 
-// 4. Fancybox
-// ---------------
-
-$('[data-fancybox="gallery"]').fancybox({
-loop: true,
-arrows: true,
-infobar: true,
-buttons: [
-    "slideShow",
-    "fullScreen",
-    "close"
-  ],
- transitionEffect: "tube",
-});
-
 // 5. Age Gate
 // -----------
 
-$(document).ready(function() {
-  if (!Cookies.get('showed_agegate')) {
-      setTimeout(function(){
-        $('#agegatesimple').foundation('open'); 
-        Cookies.set('showed_agegate', 'true', { expires: 365 });
-      },500) // 3 seconds.
-  }
-});
+
+if (!Cookies.get('showed_agegate')) {
+  setTimeout(function(){
+    $('#agegatesimple').foundation('open');
+    Cookies.set('showed_agegate', 'true', { expires: 365 });
+  },500)
+} else { 
+  $('.testing').removeClass('testing--hide');
+}
 
 $('#agegatesimple .close-button-test').click(function() {
-  $('#agegatesimple').foundation('close'); 
+  $('#agegatesimple').foundation('close');
+  $('.testing').removeClass('testing--hide');
 });
+
+
+// 4. Fancybox
+// ---------------
+
+if (document.querySelector('#gallery')) {
+  $('[data-fancybox="gallery"]').fancybox({
+  loop: true,
+  arrows: true,
+  infobar: true,
+  buttons: [
+      "slideShow",
+      "fullScreen",
+      "close"
+    ],
+   transitionEffect: "tube"
+  });
+}
 
 // 5. Carousel
 // -----------
 
-new Swiper('.carousel--hero', {
+if (document.querySelector('#carousel--hero')) {
+new Swiper('#carousel--hero', {
   effect: 'fade',
   slidesPerView: '1',
   centeredSlides: true,
@@ -65,10 +82,97 @@ new Swiper('.carousel--hero', {
     delay: 2500
   }
 })
+}
 
 // 6. Find Beer Button
 // -------------------
 
-$(".findbeer").click(function(){
+if (document.querySelector('#findbeer')) {
+$("#findbeer").click(function(){
   $(".mapboxgl-ctrl-geolocate").click()
 });
+}
+
+// 6. Map
+// ------
+
+if (document.querySelector('#map')) {
+
+  mapboxgl.accessToken = "pk.eyJ1IjoiZ2FyZXRoam9obnNkZXNpZ24iLCJhIjoiY2sxbnV1NGhmMGE4YTNvb2l1aGp5ano3ZiJ9.g6KdDp-CdRZI2KzanoxFQw";
+  
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/garethjohnsdesign/ck9e839pj28z31imtu8hqevj0/draft',
+    center: [-0.0800221, 51.5242972],
+    zoom: 12
+  });
+ 
+  // add markers to map
+  geojson.features.forEach(function(marker) {
+
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h6 class="primary-color secondary-font margin-bottom-1">' + marker.properties.title + '</h6><div class="primary-color">' + marker.properties.description + '</div>'))
+    .addTo(map);
+  });
+
+  map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+  map.addControl(new mapboxgl.GeolocateControl ({positionOptions: {enableHighAccuracy: true},trackUserLocation: true}), 'top-left');
+  
+  // disable map zoom when using scroll
+  map.scrollZoom.disable();
+
+}
+
+// 7. Animate on Scroll
+// --------------------
+
+$(function() {
+  AOS.init({ 
+   offset: 32,
+   easing: 'ease-in-out-quart', 
+   duration: 600
+   });   
+});
+
+$(function() {
+window.addEventListener('load', AOS.refresh);
+});
+
+
+// End Loop
+}
+
+// 8. Page Transitions
+// -------------------
+const options = {
+  animationSelector: '[class*="swup-transition-"]',
+  containers: [ '#swup-body' ],
+  plugins: [
+    new SwupPreloadPlugin(),
+    new SwupBodyClassPlugin(),
+    new SwupGaPlugin(),
+    new SwupScrollPlugin({
+        doScrollingRightAway: false,
+        animateScroll: true,
+        scrollFriction: 0.3,
+        scrollAcceleration: 0.04
+    })
+  ]
+};
+
+const swup = new Swup( options );
+
+// 9. Run Once
+// -----------
+init();
+
+swup.on( 'contentReplaced', init );
+
+} );
